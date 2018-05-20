2b935bbe5839fc86be9eba0bb9cdc26c347fca64
LEGOALLEC <ewan.le-goallec6@etu.univ-lorraine.fr>
2018-05-20 18:59:20 +0200

    Ajout du bootstrap sur la page de connexion

--- resources/views/login.php
+++ resources/views/login.php
@@ -1,31 +1,46 @@
 <?php include("header.php"); ?>
+	<div class="d-flex justify-content-center align-items-center container">
+		<div class="col-12">
+			<div class="row">
+				<h1>Bienvenue sur TODO List</h1>
+			</div>
+			
+			<div class="row">
+				<div id="login">
+					<?php 
+						if(Session::has('erreurConnexion')) {
+							echo "<p>".Session::get('erreurConnexion')."</p>";
+						}
+					?>
+					<form action="" method="post">
+						<div class="form-group">
+							<label for="user">Pseudo: </label><br>
+							<input type="text" placeholder="Pseudo" name="user">
+						</div>
+						<div class="form-group">	
+							<label for="mdp">Mot de passe: </label><br>
+							<input type="password" placeholder="Mot de passe" name="mdp">
+							<small class="form-text text-muted"> <a href="lostpw"> Mot de passe oublié ? </a></small><br>
+						</div>
 
-	<h1>Bienvenue sur TODO List</h1>
-	
-	<div id="login">
-		<?php 
-			if(Session::has('erreurConnexion')) {
-				echo "<p>".Session::get('erreurConnexion')."</p>";
-			}
-		?>
-		<form action="" method="post">
-			<table>
-				<tr><td><label for="user">Pseudo: </label></td><td><input type="text" placeholder="Pseudo" name="user"></td></tr>
-				<tr><td><label for="mdp">Mot de passe: </label></td><td><input type="password" placeholder="Mot de passe" name="mdp"></td></tr>
-			</table>
-			<input type="submit" id="connexion" value="Connexion"><br>
-			<a href="lostpw"> Mot de passe oublié ? </a><br>
-			<?php echo csrf_field(); ?>
+						<input type="submit" id="connexion" class="btn btn-primary" value="Connexion"><br>
+						
+						<?php echo csrf_field(); ?>
 
-		</form>
-	</div>
-				
-		<div id="signup">
-			<input type="button" onclick="location.href='inscription';" value="Inscription" />
+					</form>
+				</div>
+			</div>
+
+			<div class="row">			
+				<div id="signup" style="padding-right: 10px;">
+					<input type="button" class="btn btn-primary" onclick="location.href='inscription';" value="Inscription" />
+				</div>
+
+				<div id="anon">
+					<input type="submit" class="btn btn-primary" id="anon" value="Accès Anonyme">
+				</div>
+			</div>
 		</div>
-
-	<div id="anon">
-		<input type="submit" id="anon" value="Accés Anonyme">
 	</div>
 	
 	
