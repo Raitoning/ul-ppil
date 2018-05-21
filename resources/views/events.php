8559bcadb4fa764fb0277b512d044a7b28de8eee
LEGOALLEC <ewan.le-goallec6@etu.univ-lorraine.fr>
2018-05-21 19:31:06 +0200

    bootstrap sur la liste des événements

--- resources/views/events.php
+++ resources/views/events.php
@@ -1,33 +1,36 @@
 <?php include("header.php"); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-6">
+				<div id="mes_evenements">
 
-
-<div id="mes_evenements">
-
-	   <h1>Mes Evenements </h1>
-	   <a href="event"> Test Evenement</a><br>
-	   <?php
-		//TODO: affichage evenements
-		/*use App\Http\Controllers\ControllerConnexion;
-		$event = ControllerConnexion::getEventUtil();
-		echo $event->first();
-		foreach($event as $temp){
-			echo $temp;
-		}*/
-		?>
-</div>
-
-<div id="evenements_publics">
-
-   <h1>Evenements Publics</h1>
-   <?php
-		//TODO: affichage evenements publics
-		/*use App\Http\Controllers\Controller;
-		$event = ;
-		echo $event->first();
-		foreach($event as $temp){
-			echo $temp;
-		}*/
-	?>
-</div>
+					   <h1>Mes Evenements </h1>
+					   <a href="event"> Test Evenement</a><br>
+					   <?php
+						//TODO: affichage evenements
+						/*use App\Http\Controllers\ControllerConnexion;
+						$event = ControllerConnexion::getEventUtil();
+						echo $event->first();
+						foreach($event as $temp){
+							echo $temp;
+						}*/
+						?>
+				</div>
+			</div>
+			<div class="col-6">
+				<div id="evenements_publics">
+				   <h1>Evenements Publics</h1>
+				   <?php
+						//TODO: affichage evenements publics
+						/*use App\Http\Controllers\Controller;
+						$event = ;
+						echo $event->first();
+						foreach($event as $temp){
+							echo $temp;
+						}*/
+					?>
+				</div>
+		</div>
+	</div>
 
 <?php include("footer.php");?>
