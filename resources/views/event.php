09972a6b4b6db669e05e77aab5d406ff35e4abeb
LEGOALLEC <ewan.le-goallec6@etu.univ-lorraine.fr>
2018-05-21 19:45:45 +0200

    bootstrap sur page événement

--- resources/views/event.php
+++ resources/views/event.php
@@ -1,66 +1,70 @@
 <?php include("header.php"); ?>
+<div class="container">
+	<div class="row">
+		<label>Nom événement</label>
+	</div>
+	<div id="Desc" class="row">
+		<label>Descriptif de l'événement</label>
 
-<h1>Nom événement</h1>
+		<?php
+			//TODO: affichage descriptif evenement
+			/*use App\Http\Controllers\ControllerEvenement;
+			$desc = ControllerConnexion::getDesc();
+			echo $desc ;
+			*/
+		?>
+	</div>
 
-<input type="button" onclick="location.href='modifEvent';" value="Modifier l'événement" />
-<div id="Desc">
-	<h2>Descriptif de l'événement</h2>
+	<div id="Lieu" class="row">
+		<label>Cet événement aura lieu a :</label>
 
-	<?php
-		//TODO: affichage descriptif evenement
-		/*use App\Http\Controllers\ControllerEvenement;
-		$desc = ControllerConnexion::getDesc();
-		echo $desc ;
-		*/
-	?>
+		<?php
+			//TODO: affichage lieu evenement
+			/*use App\Http\Controllers\ControllerEvenement;
+			$desc = ControllerConnexion::getLieu();
+			echo $desc ;
+			*/
+		?>
+
+	</div>
+
+	<div id="dateDebut" class="row">
+		<label>Date de début :</label>
+		<?php
+			//TODO: affichage date evenement
+			/*use App\Http\Controllers\ControllerEvenement;
+			$desc = ControllerConnexion::getDate();
+			echo $desc ;
+			*/
+		?>
+		<br>
+	</div>
+	<div id="dateFin" class="row">
+		<label>Date de fin :</label><br>
+	</div>
+
+
+	<div id="tâches" class="row">
+		<label>Ensemble des tâches</label>
+		
+		<?php
+			//TODO: affichage tâches de l'evenement
+			/*use App\Http\Controllers\ControllerEvenement;
+			$desc = ControllerConnexion::getTasks();
+			echo $desc ;
+			*/
+		?>
+		<br>
+		
+	</div>
+
+	<div class="row">
+		<div class="col-2">
+			<input type="button" class="btn btn-primary" onclick="location.href='newTask';" value="Ajouter une tâche" />
+		</div>
+		<div class="col-1">
+			<input type="button" class="btn btn-primary" onclick="location.href='modifEvent';" value="Modifier l'événement" />
+		</div>
+	</div>
 </div>
-
-<div id="Lieu">
-	<h3>Lieu de l'événement</h3>
-
-	Cet événement aura lieu a :
-
-	<?php
-		//TODO: affichage lieu evenement
-		/*use App\Http\Controllers\ControllerEvenement;
-		$desc = ControllerConnexion::getLieu();
-		echo $desc ;
-		*/
-	?>
-
-</div>
-
-<div id="date">
-	<h4> Dates</h4>
-
-	Date de début : 
-	<?php
-		//TODO: affichage date evenement
-		/*use App\Http\Controllers\ControllerEvenement;
-		$desc = ControllerConnexion::getDate();
-		echo $desc ;
-		*/
-	?>
-	<br>
-	
-	Date de fin   :
-</div>
-
-
-<div id="tâches">
-	<h4> Tâches</h4>
- 
-	<input type="button" onclick="location.href='newTask';" value="Ajouter une tâche" />
-	
-	<?php
-		//TODO: affichage tâches de l'evenement
-		/*use App\Http\Controllers\ControllerEvenement;
-		$desc = ControllerConnexion::getTasks();
-		echo $desc ;
-		*/
-	?>
-	<br>
-	
-</div>
-
 <?php include("footer.php");?>
