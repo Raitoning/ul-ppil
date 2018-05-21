f5fe01e1e69b0b32ab87f0ed886252e5b4c01a99
LEGOALLEC <ewan.le-goallec6@etu.univ-lorraine.fr>
2018-05-21 18:05:05 +0200

    Bootstrap sur l'accueil

--- resources/views/accueil.php
+++ resources/views/accueil.php
@@ -2,31 +2,44 @@
     include 'header.php';
 ?>
 
-<div id="content">
+<div class="container">
+	<div class="row">
+		<!--<div id="content">-->
+			<div class="col-6">
+				<div class="row">
+					<a href="contacts">Contacts</a><br>
+				</div>
 
-    <div id="user_actions">
-        <a href="contacts">Contacts</a><br>
-        <a>Evènements</a><br>
-        <a href="account">Options du compte</a><br>
-        <a>Notifications</a><br>
-    </div>
+				<div class="row">
+					<a>Evènements</a><br>
+				</div>
 
-    <div id="user_lists">
-	    <p>Evenements à venir</p>
-	<?php
-		//TODO: affichage evenement
-		/*use App\Http\Controllers\ControllerConnexion;
-		$event = ControllerConnexion::getEventUtil();
-		echo $event->first();
-		foreach($event as $temp){
-			echo $temp;
-		}*/
-	?>
+				<div class="row">
+					<a href="account">Options du compte</a><br>
+				</div>
 
-    </div>
+				<div class="row">
+					<a>Notifications</a><br>
+				</div>
+			</div>
 
+		    <div class="col-6">
+			    <div id="user_lists">
+				    <p>Evenements à venir</p>
+					<?php
+						//TODO: affichage evenement
+						/*use App\Http\Controllers\ControllerConnexion;
+						$event = ControllerConnexion::getEventUtil();
+						echo $event->first();
+						foreach($event as $temp){
+							echo $temp;
+						}*/
+					?>
+				</div>
+		    </div>
+	    <!--</div>-->
+	</div>
 </div>
-
 <?php
     include 'footer.php';
 ?>
--- resources/views/header.php
+++ resources/views/header.php
@@ -3,22 +3,30 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="../../public/css/main.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 	<title>TODOList</title>
 </head>
 
 <body>
-	<div id="top_bar">
-		<a href="accueil"><div class="sName">TODO<span>List</span></div></a>
-		
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="accueil">TODO List</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+		<div id="top_bar">		
 		<?php 
-		if(Session::has('utilisateur')){
-		echo '<form action="deconnexion" method="post">
-			<div class="register">
-				<input type="submit" id="deconnexion" value="deconnexion"><br>
-				'. csrf_field() .'
-			</div>
-		</form>';
-		}
+			if(Session::has('utilisateur')){
+				echo '<form class="form-inline my-2 my-lg-0" action="deconnexion" method="post">
+					<div class="register">
+						<input type="submit" id="deconnexion" value="deconnexion"><br>
+						'. csrf_field() .'
+					</div>
+				</form>';
+			}
 		?>
-	</div>
+		</div>
+	</nav>
+
+	
 
