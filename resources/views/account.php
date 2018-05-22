462fd2f982150222e88c4ad517dd3ede0af722b7
LEGOALLEC <ewan.le-goallec6@etu.univ-lorraine.fr>
2018-05-22 17:40:05 +0200

    bootstrap sur le formulaire de modification des infos du comptes

--- resources/views/account.php
+++ resources/views/account.php
@@ -1,7 +1,7 @@
 <?php
 	include 'header.php';
 ?>
-
+<div class="container">
 	<h1>Gestion du Compte</h1>
 	<div id="content">
 		<?php if(Session::has('erreurUpdate')){
@@ -10,10 +10,22 @@
 		}?>
 		<form action="" method="post">
 			<table>
-				<tr><td><label for="pseudo">Nom d'utilisateur:</label></td><td><input type="text" placeholder="Pseudo" name="pseudo" value=<?php echo Session::get('utilisateur')->pseudo; ?> required></td></tr>
-				<tr><td><label for="mail">Email:</label></td><td><input type="text" placeholder="Email" name="mail" value=<?php echo Session::get('utilisateur')->mail; ?> required></td></tr>
-				<tr><td><label for="mdp">*Nouveau mot de passe:</label></td><td><input type="password" id="mdp" placeholder="Mot de passe" name="mdp"></td></tr>
-				<tr><td><label for="cmdp">*Confirmation:</label></td><td><input type="password" id="cmdp" placeholder="Mot de passe" name="cmdp"></td></tr>
+				<tr>
+					<td><label for="pseudo">Nom d'utilisateur:</label></td>
+					<td><input type="text" placeholder="Pseudo" name="pseudo" class="form-control" value=<?php echo Session::get('utilisateur')->pseudo; ?> required></td>
+				</tr>
+				<tr>
+					<td><label for="mail">Email:</label></td>
+					<td><input type="email" placeholder="Email" name="mail" class="form-control" value=<?php echo Session::get('utilisateur')->mail; ?> required></td>
+				</tr>
+				<tr>
+					<td><label for="mdp">*Nouveau mot de passe:</label></td>
+					<td><input type="password" id="mdp" placeholder="Mot de passe" class="form-control" name="mdp"></td>
+				</tr>
+				<tr>
+					<td><label for="cmdp">*Confirmation:</label></td>
+					<td><input type="password" id="cmdp" placeholder="Mot de passe" class="form-control" name="cmdp"></td>
+				</tr>
 
 			</table>
 			<p style="font-size: 12px">*Laisser vide si vous ne voulez pas changer de mot de passe</p>
@@ -33,17 +45,17 @@
 			<label for="notifyes"> Oui</label>
 			<input type="radio" id="notifno" name="notif" value="non" <?php if(Session::get('utilisateur')->recevoirNotif == "non")echo "checked"; ?>>
 			<label for="notifno"> Non</label><br>
-			<input type="submit" id="register" value="Appliquer"><br>
+			<input type="submit" id="register" class="btn btn-primary" value="Appliquer"><br>
 		</form>
 
 		<div id="delete">
 			<form method="post" action="supprimerCompte" onsubmit="if(confirm('Etes vous sur de vouloir supprimer votre compte?')) return true; else return false;">
-				<input type="submit" id="suppracc" value="Supprimer Compte">
+				<input type="submit" id="suppracc" class="btn btn-primary" value="Supprimer Compte">
 				<?php echo csrf_field(); ?>
 			</form>
 		</div>
 	</div>
-
+</div>
 <?php
 	include 'footer.php';
 ?>
