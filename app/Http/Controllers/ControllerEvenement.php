8dd51d0e0a93b7803f662eb04a130ce469e2f421
Raitoning <Rayman54@hotmail.fr>
2018-05-24 19:35:54 +0200

    ajout vue modifs droits participants

--- app/Http/Controllers/ControllerEvenement.php
+++ app/Http/Controllers/ControllerEvenement.php
@@ -14,94 +14,94 @@ use App\models\utilisateur;
 class ControllerEvenement extends Controller
 {
 
-	public function newEvent(Request $request){
-		
-		if($request->name == "" || $request->desc == "" || $request->lieu == "" || is_null($request->dateDeb)){
-			Session::put('erreurInscription','Veuillez remplir tout les champs obligatoires.');
-			return view("newEvent");
-		}
+    public function newEvent(Request $request){
+        
+        if($request->name == "" || $request->desc == "" || $request->lieu == "" || is_null($request->dateDeb)){
+            Session::put('erreurInscription','Veuillez remplir tout les champs obligatoires.');
+            return view("newEvent");
+        }
 
-		if( strtotime($request->dateDeb) < time()-(1 * 23 * 58 * 60)){
-			Session::put('erreurInscription','La date de début ne doit pas être dépassée.');
-			return view("newEvent");
-		}
+        if( strtotime($request->dateDeb) < time()-(1 * 23 * 58 * 60)){
+            Session::put('erreurInscription','La date de début ne doit pas être dépassée.');
+            return view("newEvent");
+        }
 
-		if(! is_null($request->dateFin)){
-			if( strtotime($request->dateDeb) > strtotime($request->dateFin)){
-				Session::put('erreurInscription','La date de fin est anterieur à la date de début.');
-				return view("newEvent");
-			}
-		}
-		
-		$newEvent = new evenement;
-		$newEvent->intitule = $request->name;
-		$newEvent->description = $request->desc;
-		$newEvent->lieu = $request->lieu;
-		$newEvent->dateDebut = $request->dateDeb;
-		$newEvent->dateFin = $request->dateFin ;
-		
-		if($request->genre == "Privé"){
-			$newEvent->public = false;
-		}else{
-			$newEvent->public = true;
-		}
+        if(! is_null($request->dateFin)){
+            if( strtotime($request->dateDeb) > strtotime($request->dateFin)){
+                Session::put('erreurInscription','La date de fin est anterieur à la date de début.');
+                return view("newEvent");
+            }
+        }
+        
+        $newEvent = new evenement;
+        $newEvent->intitule = $request->name;
+        $newEvent->description = $request->desc;
+        $newEvent->lieu = $request->lieu;
+        $newEvent->dateDebut = $request->dateDeb;
+        $newEvent->dateFin = $request->dateFin ;
+        
+        if($request->genre == "Privé"){
+            $newEvent->public = false;
+        }else{
+            $newEvent->public = true;
+        }
 
-		$newEvent->notification = true ;
+        $newEvent->notification = true ;
 
-		if($request->suppr == null){
-			$newEvent->suppressionAutomatique = false ;
-		}else{
-			$newEvent->suppressionAutomatique = true ;
-		}
-		
-		$newEvent->save();
-		
-		return redirect('/')->with("Text","Nouvel événement créé !");
-		
-	}
+        if($request->suppr == null){
+            $newEvent->suppressionAutomatique = false ;
+        }else{
+            $newEvent->suppressionAutomatique = true ;
+        }
+        
+        $newEvent->save();
+        
+        return redirect('/')->with("Text","Nouvel événement créé !");
+        
+    }
 
-	public function updateEvent(Request $request, $id){
+    public function updateEvent(Request $request, $id){
 
-		if($request->suppr == null){
-			$suppr = false ;
-		}else{
-			$suppr = true ;
-		}
+        if($request->suppr == null){
+            $suppr = false ;
+        }else{
+            $suppr = true ;
+        }
 
-		if($request->genre == "Privé"){
-			$genre = false;
-		}else{
-			$genre = true;
-		}
+        if($request->genre == "Privé"){
+            $genre = false;
+        }else{
+            $genre = true;
+        }
 
-		evenement::where('evenement_id',$id)->update(['intitule'=> $request->name,'dateDebut'=> $request->dateDeb,'dateFin'=> $request->dateFin, 'suppressionAutomatique'=>$suppr,'public'=>$genre, 'description'=> $request->desc, "lieu"=> $request->lieu]);
-			
-			return redirect('/event/'.$id);
-	}
+        evenement::where('evenement_id',$id)->update(['intitule'=> $request->name,'dateDebut'=> $request->dateDeb,'dateFin'=> $request->dateFin, 'suppressionAutomatique'=>$suppr,'public'=>$genre, 'description'=> $request->desc, "lieu"=> $request->lieu]);
+            
+            return redirect('/event/'.$id);
+    }
 
-	public static function getUserEvents(){
-		//TODO patch
-		/*$utilisateur_id = Session::get('utilisateur')->utilisateur_id;
-		$user = utilisateur::where('utilisateur_id', '=', $utilisateur_id)->first();
-		foreach ($user->evenement as $event) {
-		    //Chaque evenements de l'utilisateur (variable $user->evenement) dans la variable $event 
-			echo $event->intitule ;
-		}*/
-	}
+    public static function getUserEvents(){
+        //TODO patch
+        /*$utilisateur_id = Session::get('utilisateur')->utilisateur_id;
+        $user = utilisateur::where('utilisateur_id', '=', $utilisateur_id)->first();
+        foreach ($user->evenement as $event) {
+            //Chaque evenements de l'utilisateur (variable $user->evenement) dans la variable $event 
+            echo $event->intitule ;
+        }*/
+    }
 
-	public static function getPublicsEvents(){
-		$res = array();
-		$events = evenement::where('public', '=', 1)->get();
-		foreach ($events as $event) {
-		    array_push($res,$event);;
-		}
-		return $res;
-	}
+    public static function getPublicsEvents(){
+        $res = array();
+        $events = evenement::where('public', '=', 1)->get();
+        foreach ($events as $event) {
+            array_push($res,$event);;
+        }
+        return $res;
+    }
 
-	public static function getEvent($event_id){
-		$event = evenement::where('evenement_id', '=', $event_id)->first();
-		return $event ;
-	}
+    public static function getEvent($event_id){
+        $event = evenement::where('evenement_id', '=', $event_id)->first();
+        return $event ;
+    }
 }
 
 ?>
--- routes/web.php
+++ routes/web.php
@@ -9,107 +9,168 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
- // page d'accueil
 
-Route::get('/',function(){
-  if(Session::has('utilisateur'))
-    return redirect('accueil');
-  else
-    return view('login');
+// page d'accueil
+Route::get('/',function() {
+
+    if(Session::has('utilisateur')) {
+
+        return redirect('accueil');
+    } else {
+
+        return view('login');
+    }
 });
 
 // Accès anonyme
+Route::get('/publicsEvents',function() {
 
-Route::get('/publicsEvents',function(){
-  if(Session::has('utilisateur'))
-    return redirect('accueil');
-  else
-    return view('publicsEvents');
+    if(Session::has('utilisateur')) {
+
+        return redirect('accueil');
+    } else {
+
+        return view('publicsEvents');
+    }
 });
 
 //inscription - deconnexion
+Route::get('/login',function() {
 
-Route::get('/login',function(){
-  if(Session::has('utilisateur'))
-    return redirect('accueil');
-  else
-    return redirect('/');
+    if(Session::has('utilisateur')) {
+
+        return redirect('accueil');
+    } else {
+
+        return redirect('/');
+    }
 });
 
-Route::get('/inscription',function(){
-  if(Session::has('utilisateur'))
-    return redirect('accueil');
-  else return view('signup');
+Route::get('/inscription',function() {
+
+    if(Session::has('utilisateur')) {
+
+        return redirect('accueil');
+    } else {
+
+        return view('signup');
+    }
 });
 
-Route::get('/lostpw',function(){
-  if(Session::has('utilisateur'))
-    return redirect('accueil');
-  else return view('lostpw');
+Route::get('/lostpw',function() {
+
+    if(Session::has('utilisateur')) {
+
+        return redirect('accueil');
+    } else {
+
+        return view('lostpw');
+    }
 });
 
 
 // page d'accueil de l'utilisateur
+Route::get('/accueil',function() {
 
-Route::get('/accueil',function(){
-  if(Session::has('utilisateur'))
-    return view('accueil');
-  else return redirect('login');
+    if(Session::has('utilisateur')) {
+
+        return view('accueil');
+    } else {
+
+        return redirect('login');
+    }
 });
 
-Route::get('/contacts',function(){
-  if(Session::has('utilisateur'))
-    return view('contacts');
-  else return redirect('login');
+Route::get('/contacts',function() {
+
+    if(Session::has('utilisateur')) {
+
+        return view('contacts');
+    } else {
+
+        return redirect('login');
+    }
 });
 
-Route::get('/account',function(){
-  if(Session::has('utilisateur'))
-    return view('account');
-  else return redirect('login');
+Route::get('/account',function() {
+
+    if(Session::has('utilisateur')) {
+
+        return view('account');
+    } else {
+
+        return redirect('login');
+    }
 });
 
-Route::get('/notices',function(){
-  if(Session::has('utilisateur'))
-    return view('notices');
-  else return redirect('login');
+Route::get('/notices',function() {
+
+    if(Session::has('utilisateur')) {
+
+        return view('notices');
+    } else {
+
+        return redirect('login');
+    }
 });
 
 // Evenements
+Route::get('/newEvent',function() {
 
-Route::get('/newEvent',function(){
-  if(Session::has('utilisateur'))
-    return view('newEvent');
-  else return redirect('login');
+    if(Session::has('utilisateur')) {
+
+        return view('newEvent');
+    } else {
+
+        return redirect('login');
+    }
 });
 
-Route::get('/events',function(){
-  if(Session::has('utilisateur'))
-    return view('events');
-  else return redirect('login');
+Route::get('/events',function() {
+
+    if(Session::has('utilisateur')) {
+
+        return view('events');
+    } else {
+
+        return redirect('login');
+    }
 });
 
-Route::get('/event/{event}',function($event){
-  if(Session::has('utilisateur'))
-    return view('event', ["event_id" => $event]);
-  else return redirect('login');
+Route::get('/event/{event}',function($event) {
+
+    if(Session::has('utilisateur')) {
+
+        return view('event', ["event_id" => $event]);
+    } else {
+
+        return redirect('login');
+    }
 });
 
-Route::get('/event/modifEvent/{event_id}',function($event_id){
-  if(Session::has('utilisateur'))
-    return view('modifEvent', ["event_id" => $event_id]);
-  else return redirect('login');
+Route::get('/event/modifEvent/{event_id}',function($event_id) {
+
+    if(Session::has('utilisateur')) {
+
+        return view('modifEvent', ["event_id" => $event_id]);
+    } else {
+
+        return redirect('login');
+    }
 });
 
 
 // Tâches
+Route::get('/event/newTask/{event_id}',function($event_id) {
 
-Route::get('/event/newTask/{event_id}',function($event_id){
-  if(Session::has('utilisateur'))
-    return view('newTask', ["event_id" => $event_id]);
-  else return redirect('login');
+    if(Session::has('utilisateur')) {
+
+        return view('newTask', ["event_id" => $event_id]);
+    } else {
+
+        return redirect('login');
+    }
 });
-
 
 Route::post('/','ControllerConnexion@connexion');
 Route::post('/deconnexion','ControllerConnexion@deconnexion');
