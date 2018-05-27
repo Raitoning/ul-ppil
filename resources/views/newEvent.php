af146b734c9a28de211c64e22d2416fb242f810e
LEGOALLEC <ewan.le-goallec6@etu.univ-lorraine.fr>
2018-05-27 12:50:42 +0200

    modification page creation d'evenments

--- resources/views/newEvent.php
+++ resources/views/newEvent.php
@@ -1,56 +1,68 @@
 <?php include("header.php"); ?>
 	<div class="container">
-		<div class="row">
-			<h1>Creer un nouvel événement :</h1>
-		</div>
-
-		<?php if(Session::has('erreurInscription')){
-				echo "<div class='alert alert-danger' role='alert'>"
-						.Session::get('erreurInscription').
-					"</div>";
-				Session::forget('erreurInscription');
-			}
-		?>
-
-		<div class="row">
-			<div class="col-6">
-				<form action="" method="post">
-					<div class="newEvent"><br>
-						<div class="form-group">
-							<label>Nom de l'événement :</label><br>
-							<input type="text" class="form-control" placeholder="Nom de l'événement*" name="name" maxlength="255"><br>
-						</div>
-						<div class="form-group">
-							<label>date de début* : </label><br>
-							<input type="date" class="form-control" name="dateDeb"><br>
-							<label>date de fin* : </label><br>
-							<input type="date" class="form-control" name="dateFin"><br>
-						</div>
-						<div class="form-group">
-							<label>Type : </label>
-							<input type="radio" name="genre" value="Public" checked> Public
-			  				<input type="radio" name="genre" value="Privé"> Privé<br>
-						</div>
-						<div class="form-group">
-							<label>Description :</label><br>
-							<input type="text" class="form-control" placeholder="Description*" name="desc" maxlength="250"><br>
-						</div>
-						<div class="form-group">
-							<label>Lieu : </label><br>
-							<input type="text" class="form-control" placeholder="Lieu*" name="lieu" maxlength="250"><br><br>
-						</div>
-						<div class="form-group">
-							<input type="checkbox" name="suppr" value="suppr"> Supression automatique<br>
-							<label>* champs obligatoires</label><br>
-							<?php echo csrf_field(); ?>
-						</div>
-						<button type="submit" id="register" class="btn btn-success" value="creer l'evenement">Créer l'evenement</button><br>
-
-					</div>
-				</form>
+		<div class="card md-3">
+			<div class="card-header">
+				<h1>Creer un nouvel événement :</h1>
 			</div>
-			<div class="col-6">
-				<h2>Liste de tâches</h2>
+
+			<?php if(Session::has('erreurInscription')){
+					echo "<div class='alert alert-danger' role='alert'>"
+							.Session::get('erreurInscription').
+						"</div>";
+					Session::forget('erreurInscription');
+				}
+			?>
+
+			<div class="card-body" style="display : flex; justify-content: space-between;">
+				<div class="col-4">
+					<form action="" method="post">
+						<div class="newEvent"><br>
+							<div class="form-group">
+								<label>Nom de l'événement :</label><br>
+								<input type="text" class="form-control" placeholder="Nom de l'événement*" name="name" maxlength="255"><br>
+							</div>
+							<div class="form-group">
+								<label>date de début* : </label><br>
+								<input type="date" class="form-control" name="dateDeb"><br>
+								<label>date de fin* : </label><br>
+								<input type="date" class="form-control" name="dateFin"><br>
+							</div>
+							<div class="form-group">
+								<label>Type : </label>
+								<input type="radio" name="genre" value="Public" checked> Public
+				  				<input type="radio" name="genre" value="Privé"> Privé<br>
+							</div>
+							<div class="form-group">
+								<label>Description :</label><br>
+								<input type="text" class="form-control" placeholder="Description*" name="desc" maxlength="250"><br>
+							</div>
+							<div class="form-group">
+								<label>Lieu : </label><br>
+								<input type="text" class="form-control" placeholder="Lieu*" name="lieu" maxlength="250"><br><br>
+							</div>
+							<div class="form-group">
+								<input type="checkbox" name="suppr" value="suppr"> Supression automatique<br>
+								<label>* champs obligatoires</label><br>
+								<?php echo csrf_field(); ?>
+							</div>
+							<button type="submit" id="register" class="btn btn-success" value="creer l'evenement">Créer l'evenement</button><br>
+
+						</div>
+					</form>
+				</div>
+				<div class="card md-3">
+					<div class="card-header">
+						<h2>Liste des tâches</h2>
+					</div>
+					<div class="card-body">
+						<h3>Type tâche 1</h3>
+						<input type="checkbox" name="suppr" value="suppr"> Tâche 1<br>
+						<h3>Type tâche 2</h3>
+						<input type="checkbox" name="suppr" value="suppr"> Tâche 2<br>
+						<h3>Type tâche 3</h3>
+						<input type="checkbox" name="suppr" value="suppr"> Tâche 3<br>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
