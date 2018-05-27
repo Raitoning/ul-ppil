07096df942a8b17f580d3c9a6484751e437cbecb
LEGOALLEC <ewan.le-goallec6@etu.univ-lorraine.fr>
2018-05-27 15:18:59 +0200

    modification page notifications

--- resources/views/notices.php
+++ resources/views/notices.php
@@ -1,22 +1,25 @@
 <?php include("header.php"); ?>
-<div class="container">
-	<h1>Bienvenue <?php echo Session::get('utilisateur')->pseudo; ?></h1>
-	<div>
-		<label>Liste des notifications</label>
-	</div>
-
-
-		<?php foreach ($notices as $notice) {
-			echo '	<div id="notifications" class="w-75 p-3 d-block row" style="overflow:auto;">
-					<div id="notif_1" class="alert alert-info alert-dismissible row">
-					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
-					.$notice.'<br>'.
-					'</div>	</div>';
+<div class="d-flex justify-content-center align-items-center container">
+	<div class="col-10">
+		<div class="card md-3">
+			<div class="card-header">
+				<h1>Bienvenue <?php echo Session::get('utilisateur')->pseudo; ?></h1>
+				<label>Liste des notifications</label>
+			</div>
+			<?php foreach ($notices as $notice) {
+			echo '	<div class="card md-3">
+						<div class="card-header">
+						</div>
+						<div class="card-body">
+							<div id="notif_1" class="alert alert-info alert-dismissible row">
+								<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
+								.$notice.'<br>'.
+							'</div>	
+						</div>
+					</div>';
 		}
 		?>
-
-
-
+		</div>
 </div>
 
 <?php include("footer.php");?>
