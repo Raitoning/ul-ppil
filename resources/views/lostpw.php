eceadc44ff6f59027c5ce61a25db30071918ecca
LEGOALLEC <ewan.le-goallec6@etu.univ-lorraine.fr>
2018-05-28 17:36:33 +0200

    modification recuperer pw

--- resources/views/lostpw.php
+++ resources/views/lostpw.php
@@ -1,20 +1,24 @@
 <?php
-	include 'header.php';
+	include('header.php');
 ?>
-	<div class="container">
-		<div class="row">
-			<h1>Mot de passe oublié</h1>
-		</div>
-		<div id="login" class="row">
-			<form>
-				<div class="form-group">
-					<label for="mail">Email:</label>
-					<input type="text" class="form-control" placeholder="Email" name="mail" required><br>
-					<input type="submit" id="reinit" class="btn btn-primary" value="Réinitialiser mon mot de passe"><br>
-				</div>
-			</form>
+<div class="d-flex justify-content-center align-items-center container">
+	<div class="col-10">
+		<div class="card mb-3">
+			<div class="card-header">
+				<h1>Mot de passe oublié</h1>
+			</div>
+			<div id="login" class="card-body">
+				<form>
+					<div class="form-group">
+						<label for="mail">Email:</label>
+						<input type="email" class="form-control" placeholder="Email" name="mail" required><br>
+						<input type="submit" id="reinit" class="btn btn-primary" value="Réinitialiser mon mot de passe"><br>
+					</div>
+				</form>
+			</div>
 		</div>
 	</div>
+</div>
 <?php
-	include 'footer.php';
+	include('footer.php');
 ?>
