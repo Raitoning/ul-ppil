05ef609188db4430c9b481ff2014d83768114800
benabbou2u <38208974+benabbou2u@users.noreply.github.com>
2018-05-29 01:09:57 +0200

    task Modifier

--- resources/views/task.php
+++ resources/views/task.php
@@ -1,69 +1,75 @@
-<?php
-    include 'header.php'
-?>
-
-<div class="col-2">
-    <!-- TODO: Mettre lien retour -->
-    <input type="button" class="btn btn-primary" onclick="" value="Retour" />
-</div>
-
-<div class="d-flex justify-content-center align-items-center container">
-
-    <div class="col-10">
-
-        <div class="card mb-3">
-
-            <div class="card-header">
-
-                <h1>Détails de la tâche</h1>
-            </div>
-
-            <div class="card-body">
-
-                <label>Nom de la tâche: </label>
-
                 <?php
-                    //affichage du nom de la tâche
-                    /* use App\Http\Controllers\ControllerEvenement;
-                    use App\Http\Controllers\ControllerParticipants;
-                    use App\Http\Controllers\ControllerTache;
-                    $event = ControllerEvenement::getEvent($event_id);  //TODO cas général
-                    $tasks = ControllerTache::getTask($event_id);
-                    echo $event->intitule ; */
+                    include 'header.php'
+
                 ?>
 
-                <div id="Desc">
-
-                    <label>Descriptif de la tâche:
-                        <br>
-                    </label>
-
-                    <?php
-                        //affichage descriptif de la tâche
-                        // echo $event->description ;
-                    ?>
+                <div class="col-2">
+                    <!-- TODO: Mettre lien retour -->
+                    <input type="button" class="btn btn-primary" onclick="location.href='/events'" value="Retour" />
                 </div>
 
-                <div id="quantity">
+                <div class="d-flex justify-content-center align-items-center container">
 
-                    <label>Quantité:
-                        <br>
-                    </label>
+                    <div class="col-10">
 
+                        <div class="card mb-3">
+
+                            <div class="card-header">
+
+                                <h1>Détails de la tâche</h1>
+                            </div>
+
+                            <div class="card-body">
+
+                                <label>Nom de la tâche: </label>
+
+                                <?php
+                                    //affichage du nom de la tâche
+                                     use App\Http\Controllers\ControllerEvenement;
+                                    use App\Http\Controllers\ControllerParticipants;
+                                    use App\Http\Controllers\ControllerTache;
+                                    $tacheNom = ControllerTache::getTaskNom($tache_id);
+                                    $tacheDesc = ControllerTache::getTaskDesc($tache_id);
+                                    $tacheQuantite= ControllerTache::getTaskQuantite($tache_id);
+
+
+                                  //  foreach($tasks as $task){
+                                      echo $tacheNom;
+                                    //}
+                                ?>
+
+                                <div id="Desc">
+
+                                    <label>Descriptif de la tâche:
+                                        <br>
+                                    </label>
+
+                                    <?php
+                                        //affichage descriptif de la tâche
+                                      echo $tacheDesc;
+                                    ?>
+                                </div>
+
+                                <div id="quantity">
+
+                                    <label>Quantité:
+                                        <?php  echo $tacheQuantite ;?>
+                                    </label>
+
+                                </div>
+
+                                <div id="participants">
+
+                                    <label>Participants:
+                                        <br>
+                                    </label>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <div id="participants">
-
-                    <label>Participants:
-                        <br>
-                    </label>
-
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<?php
-    include 'footer.php'
-?>
+                <?php
+                    include 'footer.php'
+                ?>
