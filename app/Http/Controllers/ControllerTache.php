addd63d9591c6548322def8d610c40c4c211cbe8
benabbou2u <38208974+benabbou2u@users.noreply.github.com>
2018-05-28 16:27:37 +0200

    correction controllerTache

--- app/Http/Controllers/ControllerTache.php
+++ app/Http/Controllers/ControllerTache.php
@@ -10,6 +10,8 @@ use Illuminate\Database\Eloquent\Model;
 use Session;
 use App\models\evenement;
 use App\models\utilisateur;
+use App\models\tache;
+
 use Illuminate\Support\Facades\DB;
 
 class ControllerTache extends Controller
@@ -24,12 +26,29 @@ class ControllerTache extends Controller
 			Session::put('erreurFormulaire','Veuillez remplir tout les champs obligatoires.');
 			return view("newTask");
 		}
-	
-		DB::table('tache')->insert(
+
+		$evenement_id = $event;
+		$typetache_id = 1;
+		$tache = new tache();
+		$tache->nom = $request->task; // Nom de la tache (max 255char)
+		$tache->description = $request->desc ; // Description de la tache (max 255char)
+		$tache->quantiteTotal = 4; // SI elle est de type quentitative (max 999999)
+		$tache->dateFin = '2018-5-30'; // SI elle est de type datedefin , FORMAT : 'YYYY-MM-DD' , Exemple '2018-5-17'
+		$tache->evenement_evenement_id = $evenement_id;
+		$tache->typetache_typetache_id = 1;
+
+		$tache->save();
+	/*	DB::table('tache')->insert(
 		    ['nom' => $request->task, 'description' => $request->desc , 'valide' => '0',
 				 'quantiteTotal' => '3', 'dateFin' => '2018-05-24', 'evenement_evenement_id' => $event,
-				 'typetache_typetache_id' => '1']
+				 'typetache_typetache_id' => $type]
 		);
+		$id_tache= DB::table('tache')->select('tache_id')->get();
+
+		DB::table('photo_tache')->insert(
+				[ 'photo_photo_id' => $id_photo, 'tache_tache_id' => $id_tache]
+		);*/
+
 		return redirect('/event/'.$event)->with("Text","Nouvel tache créé !");
 
 		}
@@ -42,8 +61,54 @@ class ControllerTache extends Controller
       }
 			return $taches ;
 		}
+		public static function supprimerTache(){
+			$tache_id = $tache->tache_id;
+      $tache = tache::where('tache_id','=',$tache_id)->first();
+      $tache->delete();
+
+		}
+		public static function affecterUserTache(){
+			$tache_id = $tache->tache_id;
+			$utilisateur_id = Session::get('utilisateur')->utilisateur_id;
+			$tache = tache::where('tache_id','=',$tache_id)->first();
+			$quentite =1 ; // Quentite de contribution si on est en type avec quentite
+			$tache->utilisateur()->attach($utilisateur_id,['quentite'=>$quentite]);
+		}
+		public static function creerText(){
+			$text = new text();
+			$text->texte = $request; // Le text a ajoutée ( /!\ max 255char)
+			$text->save();
+		}
+		public static function creerPhoto(){
+		$photo = new photo();
+		$photo->url =$request ; // L'URL de la photo ajoutée ( /!\ max 255char)
+		$photo->save();
+		}
+		public static function ajouterTextTache(){
+			$tache_id = $tache->tache_id;
+			$text_id = $text->text_id;
+			$tache = tache::where('tache_id','=',$tache_id)->first();
+			$tache->text()->attach($text_id);
+		}
 
 
+		public static function ajouterImageTache(){
+			$tache_id = $tache->tache_id;
+      $photo_id = $photo->photo_id;
+			$tache = tache::where('tache_id','=',$tache_id)->first();
+			$tache->photo()->attach($photo_id);
+		}
+		public static function listeParticTache(){
+			$tache_id = $tache->tache_id;
+			$tache = tache::where('tache_id','=',$tache_id)->first();
+			foreach ($tache->utilisateur as $participant) {
+				// Chaque $participant correspond a un objet utilisateur etant dans la tache $tache
+
+				// Si besoin la quentite de ce participant
+				$quentiteDuParticipant = $participant->pivot->quentite;
+}
+
+		}
 
 
 
