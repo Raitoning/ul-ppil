5c4bd68dd45c2a73ea1a88c7595522e97e5a376f
harryless <38209169+harryless@users.noreply.github.com>
2018-05-28 20:33:32 +0200

    Ajout du controller newTypeTache

---
+++ app/Http/Controllers/ControllerTypeTache.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Model;
+use Session;
+use App\models\evenement;
+use App\models\utilisateur;
+use App\models\typetache;
+use Illuminate\Support\Facades\DB;
+
+class ControllerTypeTache extends Controller
+{
+
+
+
+	public function newType(Request $request){
+		if($request->nom == ""){
+			Session::put('erreurFormulaire','Veuillez remplir tout les champs obligatoires.');
+			return view("newTaskType");
+		}
+
+		$typetache = new typetache();
+	$typetache->photo =$request->img ; // Le nombre de photo que doit contenir la tache (0 si aucun)
+	$typetache->texte = $request->text; // Le nombre de text que doit contenir la tache (0 si aucun)
+	if (isset($request->checkEnddate)) {
+		$typetache->datefin = 1;// 0 pour ne pas avoir une date de fin
+	}
+	else {
+		$typetache->datefin = 0;
+	}
+	if (isset($request->checkReparti)) {
+		$typetache->quantite = 1; // 0 pour ne pas avoir de quentite
+	}
+	else {
+		$typetache->quantite = 0;
+	}
+ // 0 pour ne pas avoir une date de fin
+							// 1 pour avoir une date de fin
+	$typetache->nomtypetache = $request->nom;
+							 // 1 pour avoir une quentite
+	$typetache->save();
+
+	$utilisateur_id = Session::get('utilisateur')->utilisateur_id;
+	$utilisateur = utilisateur::where('utilisateur_id','=',$utilisateur_id)->first();
+	$utilisateur->typetache()->attach($typetache->typetache_id);
+		return redirect('/accueil')->with("Text","Nouveau type de tache créé !");
+
+		}
+
+		public static function getTypeTask($event){
+			$tasks = DB::table('tache')->where('evenement_evenement_id', '=', $event)->get();
+			$taches = [];
+      foreach($tasks as $task){
+        $taches[] = $task;
+      }
+			return $taches ;
+		}
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+?>
