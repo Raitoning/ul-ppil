388e8c8f4fb535b964137f97603182ce9b503ac5
Cephalon Raitoning Prime <Rayman54@hotmail.fr>
2018-05-28 17:50:27 +0200

    Ajout quantité et participants dans vue newTask

--- resources/views/newTask.php
+++ resources/views/newTask.php
@@ -2,46 +2,79 @@
 $link = $_SERVER['PHP_SELF'];
 $event = substr($link, strrpos($link, '/') + 1);
 ?>
-<div class="container card md-3">
-	<div class="row card-header">
-		<h1>Création de tâche</h1>
-	</div>
 
-	<div class="row card-body">
-		<form action="" method="post">
-			<div class="newTask">
-				<br>
-				<div class="form-group ">
-					<label>Nom de la tâche :</label><br>
-					<input type="text" class="form-control" placeholder="Nom de la tâche" name="task"><br><br>
-				</div>
-				<div class="form-group">
-					<label>Description :</label><br>
-					<input type="text" class="form-control" placeholder="Description" name="desc"><br><br>
-				</div>
-				<div class="form-group">
-				<label>Type de la tâche : </label>
-					<select class="form-control" name="Type de tâche">
-					  <option value="typetask1">Type 1</option>
-					  <option value="typetask2">Type 2</option>
-					  <option value="typetask3">Type 3</option>
-					  <option value="typetask4">Type 4</option>
+<div class="d-flex justify-content-center align-items-center container">
+	<div class="col-10">
+		<div class="card mb-3">
+			<div class="card-header">
+				<h1>Creer une tâche:</h1>
+			</div>
 
-					  //TODO séléction des différents types de tâches
-					</select><br><br>
-				</div>
-				<div class="row">
-					<div class="col_2">
-						<input type="button" class="btn btn-primary" onclick="location.href='/event/<?php echo "$event"; ?>';" value="Retour" />
-					</div>
-					<div class="col_2">
-						<input type="submit" id="creer" class="btn btn-success" value="Creer"><br>
+			<?php if(Session::has('erreurInscription')){
+				echo "<div class='alert alert-danger' role='alert'>"
+					.Session::get('erreurInscription').
+					"</div>";
+					Session::forget('erreurInscription');
+				}
+				?>
+
+			<div class="card-body" style="display : flex; justify-content: space-around;">
+				<div class="col-6">
+					<form action="" method="post">
+						<br>
+						<div class="form-group">
+							<label>Nom de la tâche :</label>
+							<br>
+							<input type="text" placeholder="Nom de la tâche" name="task">
+							<br>
+							<br>
+						</div>
+						<div class="form-group">
+							<label>Description :</label>
+							<br>
+							<input type="text" placeholder="Description" name="desc">
+							<br>
+							<br>
+						</div>
+						<div class="form-group">
+							<label>Quantité :</label>
+							<br>
+							<input type="text" placeholder="Quantité" name="quantity">
+							<br>
+							<br>
+						</div>
+						<div class="form-group">
+							<label>Type de la tâche : </label>
+							<select name="Type de tâche">
+								<option value="typetask1">Type 1</option>
+								<option value="typetask2">Type 2</option>
+								<option value="typetask3">Type 3</option>
+								<option value="typetask4">Type 4</option>
+
+								//TODO séléction des différents types de tâches
+							</select>
+							<br>
+							<br>
+						</div>
+						<input type="button" class="btn btn-primary" onclick="location.href='/event/<?php echo " $event "; ?>';" value="Retour" />
+						<input type="submit" id="creer" class="btn btn-primary" value="Creer">
 						<?php echo csrf_field(); ?>
+				</div>
+				<div class="col-6">
+					<div class="card md-3">
+						<div class="card-header">
+							<h2>Participants</h2>
+						</div>
+						<div class="card-body">
+						</div>
 					</div>
 				</div>
 			</div>
-		</form>
+			</form>
+		</div>
 	</div>
+</div>
+</div>
 </div>
 
 <?php include("footer.php");?>
