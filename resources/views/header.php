ba9ec0ddd12272e37a97fd0a03354e743ff6ac8b
LEGOALLEC <ewan.le-goallec6@etu.univ-lorraine.fr>
2018-05-29 18:12:40 +0200

    suppression du menu hamburger

--- resources/views/header.php
+++ resources/views/header.php
@@ -23,9 +23,6 @@
 			}
 		?>
 		</div>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
 	</nav>
 
 	
