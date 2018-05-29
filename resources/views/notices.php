a547da9b71771403fcc2cf7c1e41c38884e7c310
formotiis <nosvulca@gmail.com>
2018-05-29 13:53:04 +0200

    Beauty hotfix

--- resources/views/notices.php
+++ resources/views/notices.php
@@ -15,33 +15,24 @@
 			use App\Http\Controllers\NotifController;
 			foreach ($notices as $notice) {
 				if($notice->action == "rejoindre" || ($notice->action == "ajout" && $notice->module == "evenement")){
-					echo '	<div class="card md-3">
-						<div class="card-header">
-						</div>
+					echo '
 						<div class="card-body">
 							<div id="notif_1" class="alert alert-info alert-dismissible row">
-								<a href="notices/supprimerNotif/'.$notice->notification_id.'" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
-								.NotifController::genererMessage($notice).'<br>'
+								<a href="notices/supprimerNotif/'.$notice->notification_id.'" class="close" data-dismiss="alert" aria-label="close">&times;</a> <div class="col-9">'
+								.NotifController::genererMessage($notice).' </div><div class="col-3">'
 								.'<input type="button" class="btn btn-success" onclick="location.href=\'notices/accepterNotif/'.$notice->notification_id.'\'" value="Accepter" />'
 								.'<input type="button" class="btn btn-danger" onclick="location.href=\'notices/refuserNotif/'.$notice->notification_id.'\'" value="Refuser" />
-								</div>'.
+								</div> </div>'.
 								
-							'</div>	
-						</div>
-					</div>';
+							'</div>	';
 				}else {
-					echo '	<div class="card md-3">
-						<div class="card-header">
-						</div>
+					echo '
 						<div class="card-body">
-							<div id="notif_1" class="alert alert-info alert-dismissible row">
+							<div id="notif_1" class="alert alert-success alert-dismissible row">
 								<a href="notices/supprimerNotif/'.$notice->notification_id.'" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
-								.NotifController::genererMessage($notice).'<br>'.
+								.NotifController::genererMessage($notice).
 								'</div>'.
-								
-							'</div>	
-						</div>
-					</div>';
+							'</div>	';
 				}
 			
 		}
