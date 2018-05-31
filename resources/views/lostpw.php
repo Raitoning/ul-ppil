2e37e12f25a8e23ea4cdbfd9159c4830d9b48143
Cephalon Raitoning Prime <Rayman54@hotmail.fr>
2018-05-31 17:09:11 +0200

    Responsive cu mot de passe oublié

--- resources/views/lostpw.php
+++ resources/views/lostpw.php
@@ -1,24 +1,41 @@
 <?php
 	include('header.php');
 ?>
-<div class="d-flex justify-content-center align-items-center container">
+
+<div class="col-2">
+	<input type="button" class="btn btn-primary" onclick="location.href='/login'" value="Retour" />
+</div>
+
+<br>
+
+<div class="d-flex justify-content-center container">
+
 	<div class="col-10">
-		<div class="card mb-3">
-			<div class="card-header">
-				<h1>Mot de passe oublié</h1>
+
+		<div class="card md-6">
+
+			<div class="card-header bg-info text-white">
+
+				<h5>Mot de passe oublié</h5>
 			</div>
-			<div id="login" class="card-body">
+
+			<div class="card-body">
+			
 				<form>
+
 					<div class="form-group">
+
 						<label for="mail">Email:</label>
-						<input type="email" class="form-control" placeholder="Email" name="mail" required><br>
-						<input type="submit" id="reinit" class="btn btn-primary" value="Réinitialiser mon mot de passe"><br>
+						<input type="email" class="form-control" placeholder="Email" name="mail" required>
 					</div>
+
+					<input type="submit" id="reinit" class="btn btn-primary" value="Envoyer">
 				</form>
 			</div>
 		</div>
 	</div>
 </div>
+
 <?php
 	include('footer.php');
 ?>
