89be69a368520ecd29586c00a5fa4779a28a2739
Cephalon Raitoning Prime <Rayman54@hotmail.fr>
2018-06-01 10:22:57 +0200

    Script base de donnée final

---
+++ Base_de_donnee.sql
@@ -0,0 +1,303 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 30, 2018 at 05:08 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ppil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(3) NOT NULL,
+  `utilisateur_utilisateur_id` int(3) NOT NULL,
+  `contact_contact_id` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `evenement`
+--
+
+CREATE TABLE `evenement` (
+  `evenement_id` int(3) NOT NULL,
+  `public` int(1) NOT NULL,
+  `notification` int(1) NOT NULL,
+  `intitule` varchar(255) NOT NULL,
+  `dateDebut` date NOT NULL,
+  `dateFin` date DEFAULT NULL,
+  `description` varchar(255) NOT NULL,
+  `lieu` varchar(255) NOT NULL,
+  `suppressionAutomatique` int(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `evenement_utilisateur`
+--
+
+CREATE TABLE `evenement_utilisateur` (
+  `utilisateur_utilisateur_id` int(3) NOT NULL,
+  `evenement_evenement_id` int(3) NOT NULL,
+  `droit` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `notification_id` int(3) NOT NULL,
+  `id_emetteur` int(11) NOT NULL,
+  `id_recepteur` int(11) NOT NULL,
+  `vu` int(1) NOT NULL DEFAULT '0',
+  `module` varchar(30) NOT NULL,
+  `action` varchar(30) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `id_module` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photo`
+--
+
+CREATE TABLE `photo` (
+  `photo_id` int(3) NOT NULL,
+  `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photo_tache`
+--
+
+CREATE TABLE `photo_tache` (
+  `photo_photo_id` int(3) NOT NULL,
+  `tache_tache_id` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tache`
+--
+
+CREATE TABLE `tache` (
+  `tache_id` int(3) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `valide` int(1) NOT NULL DEFAULT '0',
+  `quantiteTotal` int(6) DEFAULT NULL,
+  `dateFin` date DEFAULT NULL,
+  `evenement_evenement_id` int(3) NOT NULL,
+  `typetache_typetache_id` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tache_text`
+--
+
+CREATE TABLE `tache_text` (
+  `tache_tache_id` int(3) NOT NULL,
+  `text_text_id` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tache_utilisateur`
+--
+
+CREATE TABLE `tache_utilisateur` (
+  `tache_tache_id` int(3) NOT NULL,
+  `utilisateur_utilisateur_id` int(3) NOT NULL,
+  `quantite` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `text`
+--
+
+CREATE TABLE `text` (
+  `text_id` int(3) NOT NULL,
+  `texte` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `typetache`
+--
+
+CREATE TABLE `typetache` (
+  `typetache_id` int(3) NOT NULL,
+  `nomtypetache` varchar(40) NOT NULL,
+  `photo` int(3) NOT NULL,
+  `texte` int(3) NOT NULL,
+  `datefin` int(1) NOT NULL,
+  `quantite` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `typetache`
+--
+
+INSERT INTO `typetache` (`typetache_id`, `nomtypetache`, `photo`, `texte`, `datefin`, `quantite`) VALUES
+(1, 'Basique', 0, 0, 0, 0),
+(2, 'Quantitative', 0, 0, 0, 1),
+(3, 'Datée', 0, 0, 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `typetache_utilisateur`
+--
+
+CREATE TABLE `typetache_utilisateur` (
+  `typetache_typetache_id` int(3) NOT NULL,
+  `utilisateur_utilisateur_id` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `utilisateur_id` int(3) NOT NULL,
+  `pseudo` varchar(40) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `recevoirInvitation` varchar(40) NOT NULL DEFAULT 'ami',
+  `recevoirMail` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `evenement`
+--
+ALTER TABLE `evenement`
+  ADD PRIMARY KEY (`evenement_id`);
+
+--
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`notification_id`);
+
+--
+-- Indexes for table `photo`
+--
+ALTER TABLE `photo`
+  ADD PRIMARY KEY (`photo_id`);
+
+--
+-- Indexes for table `tache`
+--
+ALTER TABLE `tache`
+  ADD PRIMARY KEY (`tache_id`);
+
+--
+-- Indexes for table `text`
+--
+ALTER TABLE `text`
+  ADD PRIMARY KEY (`text_id`);
+
+--
+-- Indexes for table `typetache`
+--
+ALTER TABLE `typetache`
+  ADD PRIMARY KEY (`typetache_id`);
+
+--
+-- Indexes for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`utilisateur_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `evenement`
+--
+ALTER TABLE `evenement`
+  MODIFY `evenement_id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `notification_id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `photo`
+--
+ALTER TABLE `photo`
+  MODIFY `photo_id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tache`
+--
+ALTER TABLE `tache`
+  MODIFY `tache_id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `text`
+--
+ALTER TABLE `text`
+  MODIFY `text_id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `typetache`
+--
+ALTER TABLE `typetache`
+  MODIFY `typetache_id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `utilisateur_id` int(3) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
